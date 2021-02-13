63413a2cda25c5c44c2e9cc0d5f2f7576bc418d1
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-02-13 22:22:24 +0000

    edited settings.py

--- fft/settings.py
+++ fft/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['204.48.22.22','localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['157.230.214.75', 'localhost', '127.0.0.1', ]
 
 # Application definition
 
