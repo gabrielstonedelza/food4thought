c7aba10585e091a7061f9cf9fc38a6235b7b5f5f
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-02-23 17:54:31 +0000

    edited urls

--- food4thought/urls.py
+++ food4thought/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register('thoughtlist',views.ThoughtLists)
 router.register('members',views.MembersLists)
-router.register('just2day',views.Just2Day)
+# router.register('just2day',views.Just2Day)
 
 urlpatterns = [
     path('api/',include(router.urls))
