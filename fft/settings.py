d627e7f0bc393f1b428d8e9f066391a0a0cee4af
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-02-23 16:42:06 +0000

    edited settings.py file

--- fft/settings.py
+++ fft/settings.py
@@ -144,6 +144,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ]
 }
