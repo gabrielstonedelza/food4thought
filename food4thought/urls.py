4c3cc0dd82084a69738de746f4023d73b70f7bb5
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-02-23 17:21:32 +0000

    still loading old files

--- food4thought/urls.py
+++ food4thought/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('thoughtlist',views.ThoughtLists)
+router.register('members',views.MembersLists)
+router.register('just2day',views.Just2Day)
 
 urlpatterns = [
-    path('api/post2day/', views.post_today, name='post_today'),
-    path('api/post/', views.post_list, name='post_list'),
-    path('api/post/<int:id>/', views.post_detail, name='detail'),
-    path('api/post/new/', views.create_post, name='post_new'),
-    path('api/post/<int:id>/delete/', views.post_delete, name='post_delete'),
-    path('api/members/', views.all_members, name='f4t_members'),
-    path('api/members/new/', views.become_member, name='become_member'),
-    path('api/post/<int:id>/like/', views.like_post, name='like_post')
+    path('api/',include(router.urls))
 ]
