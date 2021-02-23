7a2f37c2efe9e50ff8d2c3152eca2f82f6ddd604
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-02-23 17:03:46 +0000

    edited views

--- food4thought/views.py
+++ food4thought/views.py
@@ -35,8 +35,8 @@ def create_post(request):
     serializer = PostSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.data)
 
 
 @api_view(['GET', 'DELETE'])
