9ac2d314ff509fb78294e8029cd587cb515e877c
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-03-03 09:53:27 +0000

    checking

--- food4thought/views.py
+++ food4thought/views.py
@@ -1,21 +1,25 @@
-from .models import Post, Comment, BecomeMember,Testimony
-from .serializers import PostSerializer, CommentSerializer, BecomeMemberSerializer,TestimonySerializer
+from .models import Post, Comment, BecomeMember, Testimony
+from .serializers import PostSerializer, CommentSerializer, BecomeMemberSerializer, TestimonySerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
+
 class ThoughtLists(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-date_posted')
     serializer_class = PostSerializer
+
 
 class MembersLists(viewsets.ModelViewSet):
     queryset = BecomeMember.objects.all().order_by('-date_joined')
     serializer_class = BecomeMemberSerializer
 
+
 class Just2Day(viewsets.ModelViewSet):
     queryset = Post.objects.all()[:1]
     serializer_class = PostSerializer
+
 
 class CommentOnPost(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-date_of_comment')
@@ -23,7 +27,5 @@ class CommentOnPost(viewsets.ModelViewSet):
 
 
 class TestimonyView(viewsets.ModelViewSet):
-    queryset = Testimony.objects.all().order_by('-date_of_testimony')
+    queryset = Testimony.objects.all().order_by('date_of_testimony')
     serializer_class = TestimonySerializer
-
-
