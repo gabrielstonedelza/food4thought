6b3051a8767343084a32fbc6ca78fd4fc8f33741
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-03-10 09:15:09 +0000

    static and media files deleted in production

--- fft/settings.py
+++ fft/settings.py
@@ -136,9 +136,6 @@ STATIC_URL = '/static/'
 # )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_FAILURE_VIEW = 'blog.views.csrf_failure'
